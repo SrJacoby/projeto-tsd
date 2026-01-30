019e9459c8148a2dc4b05db747cb0f8056916f67
SrJacoby <luizjacoby294@gmail.com>
2026-01-30 17:17:25 -0300

    criando a tela de notícias

---
+++ src/data/editionNewsData.ts
@@ -0,0 +1,109 @@
+export type NewsItem = {
+    id: number
+    title: string
+    summary?: string
+    date: string
+}
+
+export type EditionNewsData = {
+    featured: NewsItem[]
+    latest: NewsItem[]
+}
+
+export const editionNewsData: Record<string, EditionNewsData> = {
+    "1": {
+        featured: [
+            {
+                id: 1,
+                title: "Notícia destaque 1",
+                summary: "Descrição notícia destaque 1",
+                date: "02/02/2022"
+            },
+
+            {
+                id: 2,
+                title: "Notícia destaque 2",
+                summary: "Descrição notícia destaque 2",
+                date: "02/02/2022"
+            },
+
+            {
+                id: 3,
+                title: "Notícia destaque 3",
+                summary: "Descrição notícia destaque 3",
+                date: "02/02/2022"
+            },
+        ],
+
+        latest: [
+            {id: 1, title: "Notícia recente 1", date: "02/02/2022"},
+            {id: 2, title: "Notícia recente 2", date: "02/02/2022"},
+            {id: 3, title: "Notícia recente 3", date: "02/02/2022"},
+            {id: 4, title: "Notícia recente 4", date: "02/02/2022"}
+        ]
+    },
+
+    "2": {
+        featured: [
+            {
+                id: 1,
+                title: "Notícia destaque 1",
+                summary: "Descrição notícia destaque 1",
+                date: "02/02/2023"
+            },
+
+            {
+                id: 2,
+                title: "Notícia destaque 2",
+                summary: "Descrição notícia destaque 2",
+                date: "02/02/2023"
+            },
+
+            {
+                id: 3,
+                title: "Notícia destaque 3",
+                summary: "Descrição notícia destaque 3",
+                date: "02/02/2023"
+            },
+        ],
+
+        latest: [
+            {id: 1, title: "Notícia recente 1", date: "02/02/2023"},
+            {id: 2, title: "Notícia recente 2", date: "02/02/2023"},
+            {id: 3, title: "Notícia recente 3", date: "02/02/2023"},
+            {id: 4, title: "Notícia recente 4", date: "02/02/2023"}
+        ]
+    },
+
+    "3": {
+        featured: [
+            {
+                id: 1,
+                title: "Notícia destaque 1",
+                summary: "Descrição notícia destaque 1",
+                date: "02/09/2025"
+            },
+
+            {
+                id: 2,
+                title: "Notícia destaque 2",
+                summary: "Descrição notícia destaque 2",
+                date: "02/09/2025"
+            },
+
+            {
+                id: 3,
+                title: "Notícia destaque 3",
+                summary: "Descrição notícia destaque 3",
+                date: "02/09/2025"
+            },
+        ],
+
+        latest: [
+            {id: 1, title: "Notícia recente 1", date: "02/09/2025"},
+            {id: 2, title: "Notícia recente 2", date: "02/09/2025"},
+            {id: 3, title: "Notícia recente 3", date: "02/09/2025"},
+            {id: 4, title: "Notícia recente 4", date: "02/09/2025"}
+        ]
+    },
+}
--- src/pages/Edition/EditionNews.tsx
+++ src/pages/Edition/EditionNews.tsx
@@ -1,8 +1,48 @@
+import { useParams } from "react-router-dom"
+import styles from './EditionNews.module.css'
+import { editionNewsData } from "../../data/editionNewsData"
+
 const EditionNews = () => {
+  const {editionID} = useParams()
+  const news = editionNewsData[editionID ?? ""]
+
+  if(!news){
+    return null
+  }
+
   return (
-    <div>
-        <h2>Notícias</h2>
-        <p>Notícias da edição</p>
+    <div className={styles.page}>
+        <section className={styles.container}>
+          {/* Coluna Principal */}
+          <div className={styles.main}>
+            <h2 className={styles.title}>Notícias</h2>
+
+            {news.featured.map((item) => (
+              <article key={item.id} className={styles.card}>
+                <h3 className={styles.cardTitle}>{item.title}</h3>
+                <span className={styles.date}>{item.date}</span>
+                <p className={styles.summary}>{item.summary}</p>
+              </article>
+            ))}
+          </div>
+
+          {/* Coluna Lateral */}
+
+          <aside className={styles.sidebar}>
+            <h3 className={styles.sidebarTitle}>Últimas Notícias</h3>
+
+            <ul className={styles.list}>
+              {news.latest.map((item) => (
+                <li key={item.id} className={styles.listItem}>
+                  <span>{item.title}</span>
+                  <span className={styles.listDate}>{item.date}</span>
+                </li>
+              ))}
+
+            </ul>
+          </aside>
+
+        </section>
     </div>
   )
 }
