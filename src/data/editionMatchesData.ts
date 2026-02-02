fdb884d288c91f83d84ca1831117b108fbed3dd5
SrJacoby <luizjacoby294@gmail.com>
2026-02-01 23:43:42 -0300

    atualizando data

--- src/data/editionMatchesData.ts
+++ src/data/editionMatchesData.ts
@@ -7,90 +7,300 @@ export const editionMatchesData: Record<string, EditionMatchesData> = {
                 round: 1,
                 matches: [
                     {home: "Argentina", away: "Espanha", homeGoals: 0, awayGoals: 1},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "Alemanha", away: "Itália", homeGoals: 1, awayGoals: 0},
+                    {home: "Brasil", away: "Portugal", homeGoals: 2, awayGoals: 1},
+                    {home: "Inglaterra", away: "Uruguai", homeGoals: 2, awayGoals: 0},
                 ],
             },
             {
                 round: 2,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "Espanha", away: "Alemanha", homeGoals: 0, awayGoals: 1},
+                    {home: "França", away: "Argentina", homeGoals: 2, awayGoals: 0},
+                    {home: "Portugal", away: "Inglaterra", homeGoals: 1, awayGoals: 1},
+                    {home: "Holanda", away: "Brasil", homeGoals: 0, awayGoals: 0},
                 ],
             },
             {
                 round: 3,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "Alemanha", away: "França", homeGoals: 2, awayGoals: 1},
+                    {home: "Itália", away: "Espanha", homeGoals: 0, awayGoals: 0},
+                    {home: "Inglaterra", away: "Holanda", homeGoals: 3, awayGoals: 2},
+                    {home: "Uruguai", away: "Portugal", homeGoals: 2, awayGoals: 1},
                 ],
             },
             {
                 round: 4,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "França", away: "Itália", homeGoals: 1, awayGoals: 0},
+                    {home: "Argentina", away: "Alemanha", homeGoals: 4, awayGoals: 0},
+                    {home: "Holanda", away: "Uruguai", homeGoals: 1, awayGoals: 0},
+                    {home: "Brasil", away: "Inglaterra", homeGoals: 3, awayGoals: 1},
                 ],
             },
             {
                 round: 5,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "Itália", away: "Argentina", homeGoals: 0, awayGoals: 0},
+                    {home: "Espanha", away: "França", homeGoals: 1, awayGoals: 1},
+                    {home: "Uruguai", away: "Brasil", homeGoals: 1, awayGoals: 0},
+                    {home: "Portugal", away: "Holanda", homeGoals: 0, awayGoals: 1},
                 ],
             },
             {
                 round: 6,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "Espanha", away: "Argentina", homeGoals: 0, awayGoals: 0},
+                    {home: "Itália", away: "Alemanha", homeGoals: 1, awayGoals: 1},
+                    {home: "Portugal", away: "Brasil", homeGoals: 2, awayGoals: 2},
+                    {home: "Uruguai", away: "Inglaterra", homeGoals: 0, awayGoals: 0},
                 ],
             },
             {
                 round: 7,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "Alemanha", away: "Espanha", homeGoals: 2, awayGoals: 1},
+                    {home: "Argentina", away: "França", homeGoals: 1, awayGoals: 0},
+                    {home: "Inglaterra", away: "Portugal", homeGoals: 1, awayGoals: 1},
+                    {home: "Brasil", away: "Holanda", homeGoals: 1, awayGoals: 2},
                 ],
             },
             {
                 round: 8,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "França", away: "Alemanha", homeGoals: 0, awayGoals: 0},
+                    {home: "Espanha", away: "Itália", homeGoals: 1, awayGoals: 0},
+                    {home: "Holanda", away: "Inglaterra", homeGoals: 2, awayGoals: 0},
+                    {home: "Portugal", away: "Uruguai", homeGoals: 4, awayGoals: 0},
                 ],
             },
             {
                 round: 9,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "Itália", away: "França", homeGoals: 1, awayGoals: 0},
+                    {home: "Alemanha", away: "Argentina", homeGoals: 1, awayGoals: 0},
+                    {home: "Uruguai", away: "Holanda", homeGoals: 2, awayGoals: 0},
+                    {home: "Inglaterra", away: "Brasil", homeGoals: 2, awayGoals: 0},
                 ],
             },
             {
                 round: 10,
                 matches: [
-                    {home: "Argentina", away: "Espanha", homeGoals: null, awayGoals: null},
-                    {home: "Alemanha", away: "Itália", homeGoals: null, awayGoals: null},
-                    {home: "Brasil", away: "Portugal", homeGoals: null, awayGoals: null},
-                    {home: "Inglaterra", away: "Uruguai", homeGoals: null, awayGoals: null},
+                    {home: "Argentina", away: "Itália", homeGoals: 0, awayGoals: 1},
+                    {home: "França", away: "Espanha", homeGoals: 1, awayGoals: 0},
+                    {home: "Brasil", away: "Uruguai", homeGoals: 0, awayGoals: 1},
+                    {home: "Holanda", away: "Portugal", homeGoals: 0, awayGoals: 2},
+                ],
+            },
+        ]
+    },
+
+    "2": {
+        rounds: [
+            {
+                round: 1,
+                matches: [
+                    {home: "Holanda", away: "Inglaterra", homeGoals: 1, awayGoals: 2},
+                    {home: "Brasil", away: "Rússia", homeGoals: 1, awayGoals: 0},
+                    {home: "Espanha", away: "Portugal", homeGoals: 1, awayGoals: 2},
+                    {home: "França", away: "Itália", homeGoals: 2, awayGoals: 0},
+                    {home: "Uruguai", away: "Argentina", homeGoals: 0, awayGoals: 2},
+                ],
+            },
+            {
+                round: 2,
+                matches: [
+                    {home: "Argentina", away: "Brasil", homeGoals: 2, awayGoals: 0},
+                    {home: "Itália", away: "Holanda", homeGoals: 0, awayGoals: 0},
+                    {home: "Portugal", away: "França", homeGoals: 1, awayGoals: 0},
+                    {home: "Rússia", away: "Espanha", homeGoals: 0, awayGoals: 1},
+                    {home: "Inglaterra", away: "Uruguai", homeGoals: 2, awayGoals: 1},
+                ],
+            },
+            {
+                round: 3,
+                matches: [
+                    {home: "Holanda", away: "Portugal", homeGoals: 0, awayGoals: 2},
+                    {home: "Espanha", away: "Argentina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Rússia", homeGoals: 1, awayGoals: 0},
+                    {home: "Uruguai", away: "Brasil", homeGoals: 0, awayGoals: 0},
+                    {home: "Inglaterra", away: "Itália", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 4,
+                matches: [
+                    {home: "Rússia", away: "Holanda", homeGoals: 1, awayGoals: 0},
+                    {home: "Portugal", away: "Inglaterra", homeGoals: 2, awayGoals: 1},
+                    {home: "Itália", away: "Uruguai", homeGoals: 0, awayGoals: 0},
+                    {home: "Argentina", away: "França", homeGoals: 1, awayGoals: 2},
+                    {home: "Brasil", away: "Espanha", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 5,
+                matches: [
+                    {home: "França", away: "Brasil", homeGoals: 0, awayGoals: 1},
+                    {home: "Uruguai", away: "Espanha", homeGoals: 0, awayGoals: 0},
+                    {home: "Holanda", away: "Argentina", homeGoals: 2, awayGoals: 1},
+                    {home: "Inglaterra", away: "Rússia", homeGoals: 1, awayGoals: 0},
+                    {home: "Itália", away: "Portugal", homeGoals: 0, awayGoals: 2},
+                ],
+            },
+            {
+                round: 6,
+                matches: [
+                    {home: "Portugal", away: "Uruguai", homeGoals: 1, awayGoals: 2},
+                    {home: "Rússia", away: "Itália", homeGoals: 0, awayGoals: 0},
+                    {home: "Argentina", away: "Inglaterra", homeGoals: 0, awayGoals: 2},
+                    {home: "Brasil", away: "Holanda", homeGoals: 1, awayGoals: 0},
+                    {home: "Espanha", away: "França", homeGoals: 2, awayGoals: 0},
+                ],
+            },
+            {
+                round: 7,
+                matches: [
+                    {home: "Uruguai", away: "França", homeGoals: 1, awayGoals: 2},
+                    {home: "Holanda", away: "Espanha", homeGoals: 0, awayGoals: 1},
+                    {home: "Inglaterra", away: "Brasil", homeGoals: 0, awayGoals: 0},
+                    {home: "Itália", away: "Argentina", homeGoals: 0, awayGoals: 0},
+                    {home: "Portugal", away: "Rússia", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 8,
+                matches: [
+                    {home: "Argentina", away: "Portugal", homeGoals: 1, awayGoals: 0},
+                    {home: "Rússia", away: "Uruguai", homeGoals: 0, awayGoals: 1},
+                    {home: "Brasil", away: "Itália", homeGoals: 1, awayGoals: 1},
+                    {home: "Espanha", away: "Inglaterra", homeGoals: 0, awayGoals: 1},
+                    {home: "França", away: "Holanda", homeGoals: 2, awayGoals: 0},
+                ],
+            },
+            {
+                round: 9,
+                matches: [
+                    {home: "Itália", away: "Espanha", homeGoals: 1, awayGoals: 1},
+                    {home: "Inglaterra", away: "França", homeGoals: 2, awayGoals: 1},
+                    {home: "Uruguai", away: "Holanda", homeGoals: 2, awayGoals: 1},
+                    {home: "Portugal", away: "Brasil", homeGoals: 0, awayGoals: 1},
+                    {home: "Rússia", away: "Argentina", homeGoals: 0, awayGoals: 0},
+                ],
+            },
+        ]
+    },
+
+    "3": {
+        rounds: [
+            {
+                round: 1,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 2,
+                matches: [
+                    {home: "Santa Catarina", away: "Espanha", homeGoals: 1, awayGoals: 2},
+                    {home: "Rio Grande do Sul", away: "Minas Gerais", homeGoals: 1, awayGoals: 0},
+                    {home: "Bahia", away: "Itália", homeGoals: 0, awayGoals: 4},
+                    {home: "Pernambuco", away: "França", homeGoals: 2, awayGoals: 1},
+                    {home: "São Paulo", away: "Paraíba", homeGoals: 2, awayGoals: 2},
+                ],
+            },
+            {
+                round: 3,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 4,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 5,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 6,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 7,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 8,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 9,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 10,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
+                ],
+            },
+            {
+                round: 11,
+                matches: [
+                    {home: "Brasil", away: "Minas Gerais", homeGoals: 3, awayGoals: 0},
+                    {home: "Espanha", away: "Rio Grande do Sul", homeGoals: 1, awayGoals: 3},
+                    {home: "Paraíba", away: "Santa Catarina", homeGoals: 2, awayGoals: 0},
+                    {home: "França", away: "Bahia", homeGoals: 0, awayGoals: 1},
+                    {home: "Pernambuco", away: "São Paulo", homeGoals: 1, awayGoals: 0},
                 ],
             },
         ]
